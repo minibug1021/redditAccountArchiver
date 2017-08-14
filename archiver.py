e223bdfbb02f2f4b81755212b049c640fe747fe4
minibug <mrhoontar@gmail.com>
2017-08-14 05:02:18 -0400

    Update archiver.py

--- archiver.py
+++ archiver.py
@@ -4,12 +4,11 @@ timeNOW = time.time()
 
 r = praw.Reddit(client_id='',
                      client_secret='',
-		                 user_agent='',
+		     user_agent='',
                      password='',
                      username='')
 
-user = sys.argv[1]
-user = r.redditor('{}'.format(user))
+user = r.redditor(sys.argv[1])
 
 print('Generating database...')
 conn = sqlite3.connect('{}.db'.format(user))
@@ -17,15 +16,11 @@ c = conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS comments(permalink TEXT, subreddit TEXT, comment TEXT, score INTEGER, timestamp INTEGER, controversiality INTEGER, edited TEXT, score_hidden TEXT, gilded INTEGER, distinguished TEXT, author_flair_css_class TEXT, author_flair_text TEXT, comment_id TEXT)')
 
 print('Creating ID list...')
-commentsNEW = []
-commentsTOP = []
+comments = {}
 for comment in user.comments.new(limit=1000):
-        commentsNEW.append(comment.id)
+        comments[comment.id] = comment
 for comment in user.comments.top(limit=1000):
-        commentsTOP.append(comment.id)
-
-commentsNEW = set(commentsNEW)
-commentsTOP = set(commentsTOP)
+        comments[comment.id] = comment
 
 print('Fetching pre-existing comments...')
 existing_ids = []
@@ -33,17 +28,14 @@ c.execute("SELECT comment_id FROM comments")
 for row in c.fetchall():
 	existing_ids.append(row[0])
 
-comments = commentsNEW | commentsTOP
-comments = comments - set(existing_ids)
-comments = list(comments)
+for item in existing_ids:
+        del comments[item]
 
 print('Starting archival with {} new comments to process...'.format(len(comments)))
 
-for i in tqdm(range(len(comments))):
-        comment = r.comment('{}'.format(comments[i]))
+for id, comment in tqdm(comments.items()):
         permalink = 'reddit.com/r/{}/comments/{}//{}'.format(comment.subreddit, comment.submission, comment)
-        c.execute('INSERT INTO comments (permalink, subreddit, comment, score, timestamp, controversiality, edited, score_hidden, gilded, distinguished, author_flair_css_class, author_flair_text, comment_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                  (permalink, str(comment.subreddit), comment.body, comment.score, comment.created_utc, comment.controversiality, comment.edited, comment.score_hidden, comment.gilded, comment.distinguished, comment.author_flair_css_class, comment.author_flair_text, comment.id))
+        c.execute('INSERT INTO comments (permalink, subreddit, comment, score, timestamp, controversiality, edited, score_hidden, gilded, distinguished, author_flair_css_class, author_flair_text, comment_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',(permalink, str(comment.subreddit), comment.body, comment.score, comment.created_utc, comment.controversiality, comment.edited, comment.score_hidden, comment.gilded, comment.distinguished, comment.author_flair_css_class, comment.author_flair_text, comment.id))
         
         conn.commit()
 

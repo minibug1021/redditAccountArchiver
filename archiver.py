66429019fec419a848dd5776711676f963c93e62
minibug <mrhoontar@gmail.com>
2017-08-14 20:50:22 -0400

    added small improvements
    comment/post length, post url

--- archiver.py
+++ archiver.py
@@ -10,12 +10,11 @@ r = praw.Reddit(client_id='',
 #input from commandline
 user = r.redditor(sys.argv[1])
 
-print('Generating database...')
+print('Generating comment database...')
 #creates db file with name of user
 conn = sqlite3.connect('{}.db'.format(user))
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS comments(permalink TEXT, subreddit TEXT, comment TEXT, score INTEGER, timestamp INTEGER, controversiality INTEGER, edited TEXT, score_hidden TEXT, gilded INTEGER, distinguished TEXT, author_flair_css_class TEXT, author_flair_text TEXT, comment_id TEXT)')
-
+c.execute('CREATE TABLE IF NOT EXISTS comments(permalink TEXT, subreddit TEXT, comment TEXT, score INTEGER, timestamp INTEGER, controversiality INTEGER, edited TEXT, score_hidden TEXT, gilded INTEGER, distinguished TEXT, author_flair_css_class TEXT, author_flair_text TEXT, comment_length INTEGER, comment_id TEXT)')
 
 comments = {}
 #adds the ID and comment object of the top 1k comments from Hot and New to our comments dict.
@@ -47,14 +46,14 @@ print('Starting archival with {} new comments to process...'.format(len(comments
 #not so ghetto progress bar
 for id, comment in tqdm(comments.items()):
         permalink = 'reddit.com/r/{}/comments/{}//{}'.format(comment.subreddit, comment.submission, comment)
-        c.execute('INSERT INTO comments (permalink, subreddit, comment, score, timestamp, controversiality, edited, score_hidden, gilded, distinguished, author_flair_css_class, author_flair_text, comment_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',(permalink, str(comment.subreddit), comment.body, comment.score, comment.created_utc, comment.controversiality, comment.edited, comment.score_hidden, comment.gilded, comment.distinguished, comment.author_flair_css_class, comment.author_flair_text, comment.id))
-        
+        c.execute('INSERT INTO comments (permalink, subreddit, comment, score, timestamp, controversiality, edited, score_hidden, gilded, distinguished, author_flair_css_class, author_flair_text, comment_length, comment_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+                  ,(permalink, str(comment.subreddit), comment.body, comment.score, comment.created_utc, comment.controversiality, comment.edited, comment.score_hidden, comment.gilded, comment.distinguished, comment.author_flair_css_class, comment.author_flair_text, len(comment.body), comment.id))
         conn.commit()
-
+        
 # ↑comments↑ --- ↓posts↓
         
 print('\nGenerating post database...')
-c.execute('CREATE TABLE IF NOT EXISTS posts(permalink TEXT, subreddit TEXT, title TEXT, body TEXT, domain TEXT, is_self TEXT, score INTEGER, timestamp INTEGER, edited TEXT, gilded INTEGER, distinguished TEXT, author_flair_css_class TEXT, author_flair_text TEXT, link_flair TEXT, post_id TEXT)')
+c.execute('CREATE TABLE IF NOT EXISTS posts(permalink TEXT, subreddit TEXT, title TEXT, body TEXT, link TEXT, domain TEXT, is_self TEXT, score INTEGER, timestamp INTEGER, edited TEXT, gilded INTEGER, distinguished TEXT, author_flair_css_class TEXT, author_flair_text TEXT, link_flair TEXT, body_length INTEGER, post_id TEXT)')
 
 posts = {}
 
@@ -77,14 +76,15 @@ for item in existing_ids:
 print('Starting archival with {} new comments to process...'.format(len(posts)))
 
 for id, post in tqdm(posts.items()):
-        c.execute('INSERT INTO posts (permalink, subreddit, title, body, domain, is_self, score, timestamp, edited, gilded, distinguished, author_flair_css_class, author_flair_text, link_flair, post_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-                  ,(post.url, str(post.subreddit), str(post.title), post.selftext, post.domain, post.is_self, post.score, post.created_utc, post.edited, post.gilded, post.distinguished, post.author_flair_css_class, post.author_flair_text, post.link_flair_text, post.id))
+        permalink = 'redd.it/{}'.format(post.id)
+        c.execute('INSERT INTO posts (permalink, subreddit, title, body, link, domain, is_self, score, timestamp, edited, gilded, distinguished, author_flair_css_class, author_flair_text, link_flair, body_length, post_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+                  ,(permalink, str(post.subreddit), str(post.title), post.selftext, post.url, post.domain, post.is_self, post.score, post.created_utc, post.edited, post.gilded, post.distinguished, post.author_flair_css_class, post.author_flair_text, post.link_flair_text, len(post.selftext), post.id))
         conn.commit()
 
 seconds = time.time()-timeNOW
 m,s = divmod(seconds,60)
 h,m = divmod(m, 60)
-print('\nFinished archiving /u/{} in %d hours, %02d minutes, and %02d seconds'.format(user) % (h, m, s))
+print('Finished archiving /u/{} in %d hours, %02d minutes, and %02d seconds'.format(user) % (h, m, s))
 
 
 c.close()
